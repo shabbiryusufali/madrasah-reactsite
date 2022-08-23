9c94cf7554d4100c398541e480c97f46c60478e7
shabbiryusufali <shabyusufali@gmail.com>
2022-08-22 17:12:43 -0700

    update footer styling

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -30,14 +30,14 @@ function Footer() {
     })
 
     return (
-        <footer className="bg-blue-500 text-center rounded-t">
+        <footer className="bg-blue-900 text-center rounded-t">
             <div className="text-sm row text-center mx-auto mb-0 md:flex">
                 <div className="text-center md:w-1/3 p-7">
                     <h2 className="text-3xl font-bold">Class Times</h2>
-                    <table className="mx-auto text-sm text-left text-gray-800 dark:text-gray-900">
+                    <table className="mx-auto text-sm text-left text-blue-300">
                         <tbody>
                             <tr className="row">
-                                <td className="px-6 py-1 text-center font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                <td className="px-6 py-1 text-center font-medium  dark:text-white whitespace-nowrap">
                                     Tilawat ud Dua
                                 </td>
                                 <td className="font-mono">
@@ -45,7 +45,7 @@ function Footer() {
                                 </td>
                             </tr>
                             <tr className="row">
-                                <td className="px-6 py-1 text-center font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                <td className="px-6 py-1 text-center font-medium  dark:text-white whitespace-nowrap">
                                     Block 1
                                 </td>
                                 <td className="font-mono">
@@ -53,7 +53,7 @@ function Footer() {
                                 </td>
                             </tr>
                             <tr className="row">
-                                <td className="px-6 py-1 text-center font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                <td className="px-6 py-1 text-center font-medium  dark:text-white whitespace-nowrap">
                                     Block 2
                                 </td>
                                 <td className="font-mono">
@@ -61,7 +61,7 @@ function Footer() {
                                 </td>
                             </tr>
                             <tr className="row">
-                                <td className="px-6  text-center py-1 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                <td className="px-6  text-center py-1 font-medium dark:text-white whitespace-nowrap">
                                     Block 3
                                 </td>
                                 <td className="font-mono">
@@ -69,7 +69,7 @@ function Footer() {
                                 </td>
                             </tr>
                             <tr className="row">
-                                <td className="px-6 py-1 text-center font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                <td className="px-6 py-1 text-center font-medium dark:text-white whitespace-nowrap">
                                     Break
                                 </td>
                                 <td className="font-mono">
@@ -77,7 +77,7 @@ function Footer() {
                                 </td>
                             </tr>
                             <tr className="row">
-                                <td className="px-6  text-center py-1 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                <td className="px-6  text-center py-1 font-medium dark:text-white whitespace-nowrap">
                                     Block 4
                                 </td>
                                 <td className="font-mono">
@@ -85,7 +85,7 @@ function Footer() {
                                 </td>
                             </tr>
                             <tr className="row">
-                                <td className="px-6  text-center py-1 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                <td className="px-6  text-center py-1 font-medium  dark:text-white whitespace-nowrap">
                                     Block 5
                                 </td>
                                 <td className="font-mono">
@@ -93,7 +93,7 @@ function Footer() {
                                 </td>
                             </tr>
                             <tr className="row">
-                                <td className="px-6  text-center py-1 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                <td className="px-6  text-center py-1 font-medium  dark:text-white whitespace-nowrap">
                                     Block 6
                                 </td>
                                 <td className="font-mono">
@@ -101,7 +101,7 @@ function Footer() {
                                 </td>
                             </tr>
                             <tr className="row">
-                                <td className="px-6  text-center py-1 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                <td className="px-6  text-center py-1 font-medium  dark:text-white whitespace-nowrap">
                                     Namaaz/Lunch
                                 </td>
                                 <td className="font-mono">
@@ -109,7 +109,7 @@ function Footer() {
                                 </td>
                             </tr>
                             <tr className="row">
-                                <td className="px-6  text-center py-1 font-medium text-gray-900 dark:text-white whitespace-nowrap">
+                                <td className="px-6  text-center py-1 font-medium  dark:text-white whitespace-nowrap">
                                     Tarbiyat Barnamaj
                                 </td>
                                 <td className="font-mono">
@@ -122,20 +122,21 @@ function Footer() {
                 <div className="text-center  md:w-1/3 p-7">
                     <h2 className="text-3xl font-bold">Latest News</h2>
                     {articles.articles.slice(0, 5).map(article => {
-                        return (<p key={article.id} className="py-1"><i>{article.dateComplete}</i> - <a href={article.link} className='text-slate-700'>{article.title}</a></p>)
+                        return (<p key={article.id} className="py-1"><i>{article.dateComplete}</i> - <a href={article.link} className='text-blue-300'>{article.title}</a></p>)
                     })}
                     <br />
-                    <a href="/articles" className="bg-transparent mx-1 hover:bg-gray-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded">Read News</a>
+                    <a href="/articles" className="bg-transparent mx-1 hover:bg-blue-300 text-blue-300 font-semibold hover:text-black py-2 px-4 border border-blue-300 hover:border-transparent rounded">Read News</a>
                     <br />
                 </div>
                 <div className="text-center md:w-1/3 p-7">
                     <h2 className="text-3xl font-bold">Contact Us</h2>
                     <address>
-                        15 84 Ave<br />
+                        8430 128 St #15 <br />
                         Surrey, B.C.<br />
                         V3W 4G3, Canada<br /><br />
-                        <a href="tel:7782312152" className="bg-transparent  mx-1 hover:bg-gray-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded">Call</a> | <a href="mailto:shabz2002786@gmail.com" className="bg-transparent  mx-1 hover:bg-gray-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded">Email</a><br />
+                        <a href="tel:7782312152" className="bg-transparent  mx-1 hover:bg-blue-300 text-blue-300 font-semibold hover:text-black py-2 px-4 border border-blue-300 hover:border-transparent rounded">Call</a> | <a href="mailto:shabz2002786@gmail.com" className="bg-transparent  mx-1 hover:bg-blue-300 text-blue-300 font-semibold hover:text-black py-2 px-4 border border-blue-300 hover:border-transparent rounded">Email</a><br />
                     </address>
+                    <div></div>
                 </div>
             </div>
             <br />
--- client/src/components/Homepage.jsx
+++ client/src/components/Homepage.jsx
@@ -59,11 +59,13 @@ function Homepage() {
         <div className="m-5 md:w-1/2 ">
           {posts.array.slice(0,4).map(article => {
             return(
-              <div className="bg-slate-700 p-3 m-2 rounded" key={article.id}>
+              <div className="bg-slate-700 pb-5 text-white p-3 m-2 rounded" key={article.id}>
+                <div className="pb-2"> 
                 <h1 className="text-2xl">{article.title}</h1>
                 <h3 className="text-sm">By {article.author} On {article.dateComplete}</h3>
                 <h2 className="text-xl pb-2">{article.description}</h2>
-                <a href={article.link} className='bg-blue-500 mx-1 hover:bg-blue-900 text-white font-semibold  py-2 px-4 rounded'>Read More</a>
+                </div>
+                <a href={article.link} className='bg-blue-500 hover:bg-blue-900 text-white font-semibold py-2 px-4 rounded'>Read More</a>
                 { user.admin ?
                 <a href={article.editLink} className='bg-blue-500 mx-1 hover:bg-blue-900 text-white font-semibold  py-2 px-4 rounded'>Edit</a>
               : <></>  
